0cf34961a3c42f6a9095f60ce9b945f660730c9f
marte14 <kattymarte14@gmail.com>
2018-11-07 16:29:34 -0500

    Card game: cards work like buttons that when pressed reveal random selections. if yes/not the kingCard, reveals winning position; shows losing and winning position if bad guess.Still need new game button

--- ThreeCardMonte/ThreeCardMonte/ViewController.swift
+++ ThreeCardMonte/ThreeCardMonte/ViewController.swift
@@ -9,12 +9,102 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-  override func viewDidLoad() {
+    //outlets
+    @IBOutlet weak var stageView : UILabel!
+    
+    //button Outlets/cards
+    //add random array selected at play "start" shuffle on "new game"
+    var images = [UIImage.init(named: "threeCard"),
+                 UIImage.init(named: "threeCard"),
+                 UIImage.init(named: "kingCard")]
+    
+    
+    @IBOutlet weak var newGame : UIButton!
+    @IBOutlet weak var card1 : UIButton!
+    @IBOutlet weak var card2 : UIButton!
+    @IBOutlet weak var card3 : UIButton!
+    
+override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
+    
 
+}//startw a down set of selected choices whuch is my starting /play game. have a hidden arrays
+
+@IBAction func pickTheKing(_ sender: UIButton){
+    images = images.shuffled()
+
+    
+   //var cardChoice: (image: UIImage?, name: String )?
+   switch sender.tag {
+    case 0:
+        if images[0] == UIImage.init(named: "kingCard") {
+            card1.setImage(UIImage.init(named: "kingCard"), for: .normal)
+            stageView.text = "You Win!"
+        }else {
+            card1.setImage(UIImage.init(named: "threeCard"), for: .normal)
+                if images[1] == UIImage.init(named: "kingCard"){
+                    card2.setImage(UIImage.init(named: "kingCard"), for: .normal)
+                } else if images[2] == UIImage.init(named: "kingCard"){
+                    card3.setImage(UIImage.init(named: "kingCard"), for: .normal)
+                }
+           
+          
+            stageView.text = "You Lose!"
+        }
+        card1.isEnabled = false
+        card2.isEnabled = false
+        card3.isEnabled = false
+    
+    case 1:
+        if images[1] == UIImage.init(named: "kingCard") {
+            card2.setImage(UIImage.init(named: "kingCard"), for: .normal)
+            stageView.text = "You Win!"
+        }else {
+            card2.setImage(UIImage.init(named: "threeCard"), for: .normal)
+                if images[0] == UIImage.init(named: "kingCard"){
+                    card2.setImage(UIImage.init(named: "kingCard"), for: .normal)
+                } else if images[2] == UIImage.init(named: "kingCard"){
+                    card3.setImage(UIImage.init(named: "kingCard"), for: .normal)
+                }
+            stageView.text = "You Lose!"
+        }
+        card1.isEnabled = false
+        card2.isEnabled = false
+        card3.isEnabled = false
+    case 2:
+       if images[2] == UIImage.init(named: "kingCard") {
+            card3.setImage(UIImage.init(named: "kingCard"), for: .normal)
+            stageView.text = "You Win!"
+        }else{
+            card3.setImage(UIImage.init(named: "threeCard"), for: .normal)
+                if images[0] == UIImage.init(named: "kingCard"){
+                    card2.setImage(UIImage.init(named: "kingCard"), for: .normal)
+                } else if images[1] == UIImage.init(named: "kingCard"){
+                    card3.setImage(UIImage.init(named: "kingCard"), for: .normal)
+                }
+            stageView.text = "You Lose!"
+            //card1.setImage(UIImage.init(named: "kingCard"), for: .normal)
+        }
+        card1.isEnabled = false
+        card2.isEnabled = false
+        card3.isEnabled = false
+    
+    default:
+        print("push to play")
+    }
+
+}
+
+//all card faces are down - how ?
+//one person clicks on a choice
+//a random selector will choose out of the 2 face up choices
+//and displays
+// if it is the king
+//     disable the other cards.
+//if not the king
+//      allow to click next one
+// else print king
+//should there be a random selector for all separate cards?
 
 }
 
